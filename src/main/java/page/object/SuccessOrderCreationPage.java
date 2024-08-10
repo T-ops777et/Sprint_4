b58d3ec28ed097fefecb236e40623e4fce849668
Tatiana Gromova <tan29011987gromova@yandex.ru>
2024-08-10 23:04:01 +0300

    В проект Sprint_4 внесены изменения

---
+++ src/main/java/page/object/SuccessOrderCreationPage.java
@@ -0,0 +1,18 @@
+package page.object;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SuccessOrderCreationPage {
+    private WebDriver driver;
+    // Локатор об успешном создании заказа
+    private By successOrderMessage = By.xpath(".//div[text()='Заказ оформлен']");
+
+    public SuccessOrderCreationPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public boolean isSuccessOrderCreationMessageVisible() {
+        return driver.findElement(successOrderMessage).isDisplayed();
+    }
+}
---
+++ src/main/java/page/object/constants/OrderButton.java
@@ -0,0 +1,8 @@
+package page.object.constants;
+
+public class OrderButton {
+    public static final String TOP_BUTTON = "Верхняя кнопка заказа";
+    public static final String BOTTOM_BUTTON = "Нижняя кнопка заказа";
+}
+
+
